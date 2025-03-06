ba597e356a787c19eff724cddb10adddf5fa3088
Ben Poon <ben@bpoon.com>
2025-03-05 18:28:55 -0600

    Navbar, Home, and About Page Work

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import styles from "./page.module.scss";
 import { Anonymous_Pro, Montserrat } from 'next/font/google';
 
@@ -22,13 +23,63 @@ export default function Home() {
       ` }>
 
         <div className={styles.home}>
-          <h1>
-            Hello, World! <br/>
-            My name is <br/>
-            Ben
-          </h1>
+          <div className={styles.mainContent}>
+            <h1>
+              Hello, World! <br/>
+              My name is <br/>
+              <span>Ben</span>
+            </h1>
+            <hr/>
+            <ul className={styles.navigation}>
+              <li>About</li>
+              <li>Projects</li>
+              <li>Ramblings</li>
+              <li>Contact</li>
+            </ul>
+          </div>
+          <div className={styles.socials}>
+            <Image
+                src="/linkedin.svg"
+                alt="Linkedin logo"
+                width="50"
+                height="50"
+                priority
+            />
+            <Image
+                src="/github.svg"
+                alt="Github logo"
+                width="50"
+                height="50"
+                priority
+            />
+            <Image
+                src="/instagram.svg"
+                alt="Instagram logo"
+                width="50"
+                height="50"
+                priority
+            />
+          </div>
         </div>
 
+        <ul id={styles.navbar}>
+          <li>Home</li>
+          <li>About</li>
+          <li>Projects</li>
+          <li>Ramblings</li>
+          <li>Contact</li>
+        </ul>
+
+        <div className={styles.about}>
+          <h2>
+            About
+          </h2>
+          <div className={styles.content}>
+            <div>
+              Hello, World!,
+            </div>
+          </div>
+        </div>
       </main>
     </div>
   );
